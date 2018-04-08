4e039997ed20a36cbc32e4b72591fbc109cd7a9d
John Knepper <jknepper@Johns-MacBook-Pro.local>
2018-04-08 15:26:24 -0700

    no loner use environment

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@
 # PRIVATE KEY = PEM root key that exists in AWS IAM, and you wish to provision as the instance's
 # root key.
 # ROLE = (openvpn|kube-master|etc)
-# TAG_NAME = (openvpn|kube-master|whatever)
+# TAG_NAME = (openvpn|whatever)
 # SKIP (bool) = true or false. If true, will not spin up a new EC2 instance and will provision it 
 # with the play book specified as ROLE.
 
 if [  "$#" -ne 4 ]; then
-echo <<EOF
+echo "
 USAGE:
 
   ./run.sh \$PRIVATE_KEY_PATH \$ROLE \$TAG_NAME \$BOOL
@@ -19,13 +19,14 @@ USAGE:
   \$ROLE - the role (play) you wish to run; under ./roles/
   \$TAG_NAME - the name you wish to tag your instance with; this will automatically prefix the ENVIRONMENT
     variable set in the ansible_env file.
+  \$ENVIRONMENT - the environment; this gets joined with tag name for dynamic inventory.
   \$BOOL - Spin up a new EC2 instance or provision the old one using ansible AWS EC2 tagging in your playbook.
 
 EXAMPLE:
 
-  ./run.sh ~/.ssh/production-vpc-us-east-1.pem kube-master kube-master-test true
+  ./run.sh ~/.ssh/production-vpc-us-east-1.pem openvpn openvpn development true
    
-EOF
+"
 
 exit
 
@@ -37,8 +38,8 @@ source ansible_env
 
 export ROLE=$2
 export TAG_NAME=$3
-export TAG_ENV=${ENVIRONMENT}
-export NO_SKIP=$4
+export TAG_ENV=$4
+export NO_SKIP=$5
 
 # when set to true, we want to create an EC2 instance.  Otherwise, carry-on.
 if [ "$NO_SKIP" == "true" ]; then
@@ -58,7 +59,8 @@ export SED_ENV=`echo $TAG_ENV | sed 's/-/_/g'`
 ansible-playbook ${ROLE}.yml -i ec2.py \
                              -u ubuntu \
                              --private-key=$id_rsa \
+                             --vault-password-file ./.vault_pass \
                              -vvvv \
                              --extra-vars "tag_name=${SED_NAME} tag_environment=${SED_ENV}" \
-                             --tags "configure,deploy"
+                             --tags "configure,deploy,create"
 
